c029c550f144c62a5bd9e4c0bf2a23dad9268341
Daniel39177937 <39177937wasde@gmail.com>
2022-05-25 00:14:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/finalprojecttuesam/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.finalprojecttuesam
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.SurfaceHolder
+import com.example.finalprojecttuesam.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
