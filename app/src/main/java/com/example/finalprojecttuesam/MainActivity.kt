79e3646da63a7bfd4b9c22da29aaf5e5497fb1c9
Daniel39177937 <39177937wasde@gmail.com>
2022-05-27 01:40:44 +0800

    V3

--- app/src/main/java/com/example/finalprojecttuesam/MainActivity.kt
+++ app/src/main/java/com/example/finalprojecttuesam/MainActivity.kt
@@ -6,8 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
 import com.example.finalprojecttuesam.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -20,6 +25,13 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val img :ImageView=binding.myPic
+        Glide.with(this)
+            .load(R.drawable.mypic)
+            .circleCrop()
+            .override(800, 600)
+            .into(img)
+
         surface=binding.mysv
         pic=binding.start
         pic.setOnClickListener(object:View.OnClickListener{
